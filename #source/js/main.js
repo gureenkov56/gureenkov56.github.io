a75afd50877603083d58c58bae36e19e2a2d65c4
gureenkov56 <gureenkov56@gmail.com>
2021-06-28 23:40:47 +1000

    Add text JS-animation on scroll

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,17 +1,38 @@
-const text = "WEB-DEVELOPER";
+$(window).on("load", () =>
+  setTimeout(textAnimate("WEB-DEVELOPER", $("h1 > span")), 5000)
+);
 
-const textArr = text.split("");
-const spanText = $(".cursor");
+let wasMyTools = false;
+$(window).scroll(() => {
+  if (
+    $(document).scrollTop() + 450 > $(".my-tools").offset().top && wasMyTools === false
+  ) {
+    textAnimate("MY TOOLS", $(".my-tools > h2 > span"));
+    wasMyTools = true;
+  }
+});
 
-function textAnimate(txtArr) {
+let wasMyProject = false;
+$(window).scroll(() => {
+  if (
+    $(document).scrollTop() + 450 > $(".projects").offset().top &&
+    wasMyProject === false
+  ) {
+    textAnimate("PROJECTS", $(".projects > h2 > span"));
+    wasMyProject = true;
+  }
+});
+
+function textAnimate(text, htmlEl) {
+  let txtArr = text.split("");
   let i = 0;
   let timer = setInterval(() => {
     if (i + 1 >= txtArr.length) {
       clearInterval(timer);
     }
-    spanText.before(txtArr[i]);
+    htmlEl.before(txtArr[i]);
     i++;
   }, 40);
 }
 
-$(window).on("load", () => setTimeout(textAnimate(textArr), 5000));
+
