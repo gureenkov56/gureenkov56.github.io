3736b0ef36583292f30e2525fdd6a6638eac38b2
gureenkov56 <gureenkov56@gmail.com>
2021-06-28 21:49:46 +1000

    H1 animated

---
+++ #source/js/main.js
@@ -0,0 +1,17 @@
+const text = "WEB-DEVELOPER";
+
+const textArr = text.split("");
+const spanText = $(".cursor");
+
+function textAnimate(txtArr) {
+  let i = 0;
+  let timer = setInterval(() => {
+    if (i + 1 >= txtArr.length) {
+      clearInterval(timer);
+    }
+    spanText.before(txtArr[i]);
+    i++;
+  }, 30);
+}
+
+textAnimate(textArr);
